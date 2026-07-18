2dfdac4e4313f61332c9b7403149a1cca08eb5de
SmokyMs <monetomane22@gmail.com>
2026-07-18 21:02:33 +0100

    Add site footer (#1)

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,24 @@
+function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="footer-links">
+        <a
+          href="https://github.com/SmokyMs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://www.linkedin.com/in/vladislav-gavronski-800a4b21b/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
